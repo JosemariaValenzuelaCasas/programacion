4b3a5242a3f31058b9bb248bf0ba25fb1c55aedb
JosemariaValenzuela <jose.valenzuela.casas@vallegrande.edu.pe>
2025-03-28 08:24:51 -0500

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,8 @@
+CREATE TABLE customer (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    dni CHAR(8) NOT NULL,
+    first_name NVARCHAR(100) NOT NULL,
+    last_name NVARCHAR(100) NOT NULL,
+    state CHAR(1) NOT NULL
+);
 
