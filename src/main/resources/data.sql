fc0657fef38c947ba064f0106212973a95afa8cb
JosemariaValenzuela <jose.valenzuela.casas@vallegrande.edu.pe>
2025-04-12 23:08:09 -0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO guest (user_id, name, surname, identity_document, document_type, cellphone, email)
-VALUES (1, 'Juan', 'Pérez', '12345678', 'DNI', '987654321', 'juan@example.com');
+VALUES (1, 'Juan', 'Pérez', '12345678', 'DNI', '987654321', 'juan@delfines.com');
 
 
